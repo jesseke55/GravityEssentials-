52ca8bc718d1d1759b0761a13e558c8978ba9449
Ronald Bunk <Experminator@users.noreply.github.com>
2015-12-18 15:54:54 +0100

    Create PluginUtil.java

---
+++ src/net/GravityNetwork/Essentials/util/PluginUtil.java
@@ -0,0 +1,32 @@
+package net.GravityNetwork.Essentials.util;
+
+public class PluginUtil {
+
+  private PluginUtil() { }
+  
+  public static void reload(ReloadType type) {
+      switch(type) {
+        case PLUGIN:
+          Bukkit.getServer().getPluginManager().disablePlugin(Main.getPlugin());
+          Bukkit.getServer().getPluginManager().enablePlugin(Main.getPlugin());
+          break;
+        case CONFIG:
+          Main.getPlugin().reloadConfig();
+          break;
+        case ALL:
+          Main.getPlugin().reloadConfig();
+          Bukkit.getServer().getPluginManager().disablePlugin(Main.getPlugin());
+          Bukkit.getServer().getPluginManager().enablePlugin(Main.getPlugin());
+          break;
+        default:
+          throw new IllegalArgumentException("Invalid reload type.");
+      }
+  }
+  
+  public static enum ReloadType {
+  
+      PLUGIN,
+      CONFIG,
+      ALL;
+  }
+}
